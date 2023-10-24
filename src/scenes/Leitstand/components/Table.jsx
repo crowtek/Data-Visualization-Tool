4de75576d75d0e0c2c0ede5a6a115eb307a34024
crowtek <prt3@hotmail.de>
2023-10-24 15:05:38 +0200

    Table Info component umbenannt

--- src/scenes/Leitstand/components/Table.jsx
+++ src/scenes/Leitstand/components/Table.jsx
@@ -4,7 +4,7 @@ import { Box } from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { TableStyle as TableStyleImport} from "../../../styles/TableStyle";
 import VehicalModal from "./Modal";
-import VehicleInfo from "../../../components/VehicleInfo";
+import VehicleInfo from "../../../components/TableInfo";
 import VehicleStatus from "./VehicleStatus";
 
 
