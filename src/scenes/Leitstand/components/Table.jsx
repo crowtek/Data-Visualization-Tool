0c2a7440c8a54c7e6509ea21d311b57cdb10f1b3
crowtek <prt3@hotmail.de>
2023-10-29 00:23:40 +0200

    Neue daten in Leitstand Table hinzugefügt

--- src/scenes/Leitstand/components/Table.jsx
+++ src/scenes/Leitstand/components/Table.jsx
@@ -16,10 +16,14 @@ const VehicleOverviewTable = ({ userData }) => {
     const columns = useMemo(() => {
       return [
         { field: "status", headerName: "Status",flex: 0.3,renderCell: ({ row: { status } }) => <VehicleStatus status={status}/>,},
-        { field: "name", headerName: "Vehicle ID",flex: 0.5 , id: "id"},
-        { field: "parkPlace",headerName: "Park Place",flex: 0.3,editable: true},
-        { field: "takeAppointment",headerName: "Take Appointment",flex: 0.5,editable: true},
-        { field: "info",headerName: "Info",flex: 2,renderCell: ({ row: { info} }) => <VehicleInfo info={info}/>},
+        { field: "vehicle", headerName: "Kennzeichen",flex: 0.5 , id: "id"},
+        { field: "parkPlace",headerName: "Standort",flex: 0.3,editable: true},
+        { field: "cargo",headerName: "Ladeeinheit",flex: 0.5,editable: true},
+        { field: "relation",headerName: "Relation",flex: 0.5,editable: true},
+        { field: "typ",headerName: "Typ",flex: 0.5,editable: true},
+        { field: "land",headerName: "Land",flex: 0.5,renderCell: ({ row }) => row.land.kurz,},
+        { field: "customer",headerName: "Kunde",flex: 0.5,renderCell: ({ row }) => row.customer.name,},
+        { field: "info",headerName: "Info",flex: 1,editable: true},
         { field: "options",headerName: "",renderCell: ({ row }) => <VehicalModal {...row}/>,},
       ];
     },[])
