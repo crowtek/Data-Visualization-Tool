5a574b9b3aef12cb27aa73f017e90f14e884a5fd
crowtek <prt3@hotmail.de>
2023-10-28 23:23:56 +0200

    Code sauber gemacht und condition eingebaut so das nur 9 elemente in dashboard Liste angezeigt werden

--- src/scenes/Dashboard/components/Liste.jsx
+++ src/scenes/Dashboard/components/Liste.jsx
@@ -1,14 +1,10 @@
 import { Box, Typography, useTheme } from "@mui/material";
 import { tokens } from "../../../theme";
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
-import { Link } from 'react-router-dom';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 
 
 const LatestIncomingCarsList = ({ vehicleData,screenSize }) => {
@@ -16,17 +12,19 @@ const LatestIncomingCarsList = ({ vehicleData,screenSize }) => {
   const colors = tokens(theme.palette.mode);
 
   return (
-    <Box gridColumn="span 4" gridRow="span 3" backgroundColor={colors.primary[400]} overflow="auto" className="custom-scrollbar"
-    borderRadius="5px" border="2px solid gray">
+    <Box 
+      gridColumn="span 4" 
+      gridRow="span 3" 
+      backgroundColor={colors.primary[400]} 
+      borderRadius="5px"
+      border="2px solid gray"
+    >
       
-      <Box>
-        <Typography color={colors.grey[100]} variant="h3" fontWeight="bold"  p={2}>
-          Neuste aufgenommene Ladeeinheiten
-        </Typography>
-      </Box>
+      <Typography color={colors.grey[100]} variant="h3" fontWeight="bold"  p={2}>
+        Neuste aufgenommene Ladeeinheiten
+      </Typography>
 
-      {/* MAX. 10 Einbauen */}
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table>
         <TableHead>
           <TableRow > 
             <TableCell sx={{fontSize:"20px",fontWeight:"bold"}}>Kennzeichen</TableCell>
@@ -35,13 +33,17 @@ const LatestIncomingCarsList = ({ vehicleData,screenSize }) => {
           </TableRow>
         </TableHead>
         <TableBody>
-        {vehicleData.map((car, i) => (
-          <TableRow key={i}>
-            <TableCell >{car.name}</TableCell>
-            <TableCell>{car.incomingTime}</TableCell>
-            <TableCell>{car.parkPlace}</TableCell>
-          </TableRow>
-        ))}
+        {vehicleData.map((car, i) => {
+          if(i <= 9){
+            return(
+              <TableRow key={i}>
+                <TableCell >{car.name}</TableCell>
+                <TableCell>{car.incomingTime}</TableCell>
+                <TableCell>{car.parkPlace}</TableCell>
+              </TableRow>
+            )
+          }
+        })}
 
         </TableBody>
       </Table>
