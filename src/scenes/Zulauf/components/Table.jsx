15ef5345e87bf0d42530bc735f88fda5b9817ab9
crowtek <prt3@hotmail.de>
2023-10-29 11:36:02 +0100

    Zulauf bugs behoben und table data angepasst

--- src/scenes/Zulauf/components/Table.jsx
+++ src/scenes/Zulauf/components/Table.jsx
@@ -5,6 +5,7 @@ import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { TableStyle as  TableStyleImport} from "../../../styles/TableStyle";
 import VehicalModal from "./Modal";
 import VehicleInfo from "../../../components/TableInfo";
+import VehicleStatus from "../../Leitstand/components/VehicleStatus";
 
 const IncomingVehicleTable = ({ userData }) => {
     const TableStyle = useMemo(() => {
@@ -13,12 +14,17 @@ const IncomingVehicleTable = ({ userData }) => {
 
     const columns = useMemo(() => {
       return [
-        { field: "name", headerName: "Vehicle Number",flex: 0.5 , id: "id",},
-        { field: "parkPlace",headerName: "Park Place",flex: 0.3,editable: true,},
-        { field: "takeAppointment",headerName: "Intake Appointment",flex: 0.5,editable: true},
-        { field: "info", headerName: "Info", flex: 2, renderCell: ({ row: { info } }) => <VehicleInfo info={info}/>},
-        { field: "options",headerName: "",renderCell: ({ row  }) => <VehicalModal {...row}/>,},
-      ]
+        { field: "status", headerName: "Status",flex: 0.3,renderCell: ({ row: { status } }) => <VehicleStatus status={status}/>,},
+        { field: "vehicle", headerName: "Kennzeichen",flex: 0.5 , id: "id"},
+        { field: "parkPlace",headerName: "Standort",flex: 0.3,editable: true},
+        { field: "cargo",headerName: "Ladeeinheit",flex: 0.5,editable: true},
+        { field: "relation",headerName: "Relation",flex: 0.5,editable: true},
+        { field: "typ",headerName: "Typ",flex: 0.5,editable: true},
+        { field: "land",headerName: "Land",flex: 0.5,renderCell: ({ row }) => row.land.kurz,},
+        { field: "customer",headerName: "Kunde",flex: 0.5,renderCell: ({ row }) => row.customer.name,},
+        { field: "info",headerName: "Info",flex: 1,editable: true},
+        { field: "options",headerName: "",renderCell: ({ row }) => <VehicalModal {...row}/>,},
+      ];
     },[])
 
     return (
