4219b277b37f2c911011b3a7f4590b75f9fc19fa
crowtek <prt3@hotmail.de>
2023-11-12 21:23:55 +0100

    Added a From into Erfassung

--- src/scenes/Erfassung/index.jsx
+++ src/scenes/Erfassung/index.jsx
@@ -1,7 +1,66 @@
+import { useState } from "react";
+import { TextField, Button, Box } from '@mui/material';
 
 const Erfassung = () => {
+    const [formData, setFormData] = useState({
+        name: "",
+        age: "",
+    })
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setFormData(prevFormData => ({
+            ...prevFormData,
+            [name]: value
+        }));
+    }
+    
+    function handleSubmit(event) {
+        event.preventDefault();
+        // Process formData
+        console.log(formData);
+    }
+
     return (
-        <>Erfassung</>
+        <Box width={"400px"}> 
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="username"
+                    label="Username"
+                    name="username"
+                    autoComplete="username"
+                    autoFocus
+                    value={formData.username}
+                    onChange={handleChange}
+                />
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="email"
+                    label="Email Address"
+                    type="email"
+                    id="email"
+                    autoComplete="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+                {/* Other form elements */}
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                >
+                    Submit
+                </Button>
+            </Box>
+        </Box>
     )
 }
 
