d24f4453045323edf9c3fb8cbd4172c2252bdef7
crowtek <prt3@hotmail.de>
2023-11-16 21:49:01 +0100

    added image to new user form

--- src/scenes/Erfassung/forms/NewUserForm.jsx
+++ src/scenes/Erfassung/forms/NewUserForm.jsx
@@ -1,3 +1,4 @@
+import { Box,Typography,Avatar  } from '@mui/material';
 
 import NameInput from "../components/NameInput"
 import EmailInput from "../components/EmailInput";
@@ -5,11 +6,20 @@ import PhoneNumberInput from "../components/PhoneNumberInput";
 
 const NewUserForm = ({handleChange,formData}) => {
     return (
-        <>
-            <NameInput name={formData.username} handleChange={handleChange}/>
-            <EmailInput email={formData.email} handleChange={handleChange}/>
-            <PhoneNumberInput phoneNumber={formData.phoneNumber} handleChange={handleChange}/>
-        </>
+        <Box sx={{display:"flex"}}>
+            <Box>
+                <NameInput name={formData.username} handleChange={handleChange}/>
+                <EmailInput email={formData.email} handleChange={handleChange}/>
+                <PhoneNumberInput phoneNumber={formData.phoneNumber} handleChange={handleChange}/>
+            </Box>
+            <Box>
+                <Avatar
+                    alt="Image"
+                    src="/static/images/avatar/1.jpg"
+                    sx={{ width: 124, height: 124, margin:"70px"}}
+                />
+            </Box>
+        </Box>
     )
 }
 export default NewUserForm;
