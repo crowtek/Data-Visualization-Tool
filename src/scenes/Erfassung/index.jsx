9f88b803cd568cfd3d67859626acf1489d8895a4
crowtek <prt3@hotmail.de>
2023-11-16 21:38:17 +0100

    added from title

--- src/scenes/Erfassung/index.jsx
+++ src/scenes/Erfassung/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Box } from '@mui/material';
+import { Box,Typography } from '@mui/material';
 
 import FormTypeSelect from "./components/FormTypeSelect";
 import SubmitButton from "./components/SumbmitButton";
@@ -41,6 +41,11 @@ const Erfassung = () => {
     return (
         <Box sx={{display:"flex", justifyContent:"center"}}> 
             <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1, width:"30vw" }}>
+                { 
+                    formData.formType === "Neuer Benutzer" ? (<Typography variant="h2" mb={4}>Neuen Benutzer Anlegen</Typography>):
+                    (<Typography variant="h2" mb={4}>Neue Ladeeinheit Planen</Typography>)
+                }
+
                 <FormTypeSelect formType={formData.formType} handleChange={handleChange}/>
                 {  
                     formData.formType === "Neuer Benutzer" ? (<NewUserForm handleChange={handleChange} formData={formData}/>) : 
