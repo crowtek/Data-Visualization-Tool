99d78fe6e27714cc87071309521669e54ad44598
crowtek <prt3@hotmail.de>
2023-11-17 11:16:52 +0100

    table navigations state erstellt

--- src/scenes/Leitstand/index.jsx
+++ src/scenes/Leitstand/index.jsx
@@ -1,18 +1,42 @@
 import { useState } from "react";
-import { Box,Button,Typography } from "@mui/material";
+import { Box,Button } from "@mui/material";
 import { mockDataVehicles } from "../../data/mockData";
 import VehicleOverviewTable from "./components/Table";
 
 
 const Leistand = () => {
+  const [table, setTable] = useState("LEITSTAND")
+
+  const handleTableChange = (event) => {
+    const buttonValue = event.target.innerText;
+    console.log(buttonValue)
+    setTable(buttonValue)
+  }
 
   return (
     <Box m="20px">
       <Box sx={{display:"flex", justifyContent:"flex-end", gap:5, marginBlock:2}}>
         <Box sx={{display:"flex", justifyContent:"space-between", gap:2}}>
-          <Button variant="contained">Leitstand</Button>
-          <Button variant="outlined">History</Button>
-          <Button variant="outlined">Zulauff</Button>
+          <Button 
+            variant={table === "LEITSTAND" ? "contained" : "outlined"}
+            onClick={handleTableChange}
+          >
+              Leitstand
+          </Button>
+
+          <Button
+            variant={table === "HISTORY" ? "contained" : "outlined"}
+            onClick={handleTableChange}
+          >
+            History
+          </Button>
+
+          <Button 
+            variant={table === "ZULAUF" ? "contained" : "outlined"}
+            onClick={handleTableChange}
+          >
+            Zulauf
+          </Button>
         </Box>
       </Box>
       <VehicleOverviewTable userData={mockDataVehicles}/>
