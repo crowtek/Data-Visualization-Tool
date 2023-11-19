39ae954b34e8374c19e8177f61c182930fed19fb
meik <prt3@hotmail.de>
2023-11-19 14:15:06 +0100

    dashboard magin angepasst und firebase package entfernt

--- src/scenes/Dashboard/index.jsx
+++ src/scenes/Dashboard/index.jsx
@@ -16,18 +16,18 @@ const Dashboard = () => {
   const isScreenLg = useMediaQuery(theme.breakpoints.down("lg"));
 
   return (
-    <Box m="20px">
+    <Box m="20px" mt="0px">
 
       <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gridAutoRows="repeat(6, 1fr)" gap="10px"
-        >
+      >
         <CarTotal vehicleData={mockDataVehicles} />
         <CarDamaged vehicleData={mockDataVehicles} />
         <CarToday />
         <EventToday />
         <UserOnline />
         <PlaceOverview />
-        <WeeklyOverview isScreenLg={isScreenLg}/>
-        <List vehicleData={mockDataVehicles} screenSize={isScreenLg}/>
+        <WeeklyOverview isScreenLg={isScreenLg} />
+        <List vehicleData={mockDataVehicles} screenSize={isScreenLg} />
       </Box>
 
     </Box>
