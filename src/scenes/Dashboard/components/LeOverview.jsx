8c4d121debbb00475fa6cd3716b13e89c9b8134c
crowtek <prt3@hotmail.de>
2023-11-19 22:55:15 +0100

    Ladeeinheiten Übersicht mit richtigen daten aus mockdaten angepasst

--- src/scenes/Dashboard/components/LeOverview.jsx
+++ src/scenes/Dashboard/components/LeOverview.jsx
@@ -6,13 +6,17 @@ const CountDamageCar = ({vehicleData}) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode)
 
+    const vehicleEmpty = vehicleData.filter((vehicle => vehicle.status === "empty"));
+    const vehicleFull = vehicleData.filter((vehicle => vehicle.status === "full"));
+    const vehicleTeilbeladen = vehicleData.filter((vehicle => vehicle.status === "teilbeladen"));
+
     return (
         <Box gridColumn="span 3"gridRow="span 2"  sx={{display:"flex", alignItems:"center", justifyContent:"center"}} backgroundColor={colors.primary[400]}
         borderRadius="5px" border="2px solid gray">
             <StatBox
                 title="Ladeeinheiten Übersicht"
                 labels={{first:"Volle LE", second:"Leere LE", third:"Teilbeladen LE"}}
-                chartValues={{first:20, second:30, third:40}}
+                chartValues={{first:vehicleFull.length, second:vehicleEmpty.length, third:vehicleTeilbeladen.length}}
                 animationTime={400}
             />
             
