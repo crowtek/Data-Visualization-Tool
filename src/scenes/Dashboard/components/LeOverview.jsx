3f8f927858b1580c274dc8b2a4c2eb3faf34491b
crowtek <prt3@hotmail.de>
2023-11-19 23:02:42 +0100

    added useMemo to Le Overview

--- src/scenes/Dashboard/components/LeOverview.jsx
+++ src/scenes/Dashboard/components/LeOverview.jsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import StatBox from "../../../components/StatBox";
 import { useTheme,Box } from "@mui/material";
 import { tokens } from "../../../theme";
@@ -6,17 +7,32 @@ const CountDamageCar = ({vehicleData}) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode)
 
-    const vehicleEmpty = vehicleData.filter((vehicle => vehicle.status === "empty"));
-    const vehicleFull = vehicleData.filter((vehicle => vehicle.status === "full"));
-    const vehicleTeilbeladen = vehicleData.filter((vehicle => vehicle.status === "teilbeladen"));
+
+    const filteredVehicles = useMemo(()=> {
+        const vehicleEmpty = vehicleData.filter((vehicle => vehicle.status === "empty"));
+        const vehicleFull = vehicleData.filter((vehicle => vehicle.status === "full"));
+        const vehicleTeilbeladen = vehicleData.filter((vehicle => vehicle.status === "teilbeladen"));
+        return {vehicleEmpty,vehicleFull,vehicleTeilbeladen}
+    },[vehicleData])
+
 
     return (
-        <Box gridColumn="span 3"gridRow="span 2"  sx={{display:"flex", alignItems:"center", justifyContent:"center"}} backgroundColor={colors.primary[400]}
-        borderRadius="5px" border="2px solid gray">
+        <Box 
+            gridColumn="span 3"
+            gridRow="span 2"   
+            backgroundColor={colors.primary[400]}
+            borderRadius="5px"
+            border="2px solid gray"
+            sx={{display:"flex", alignItems:"center", justifyContent:"center"}}
+        >
             <StatBox
                 title="Ladeeinheiten Übersicht"
                 labels={{first:"Volle LE", second:"Leere LE", third:"Teilbeladen LE"}}
-                chartValues={{first:vehicleFull.length, second:vehicleEmpty.length, third:vehicleTeilbeladen.length}}
+                chartValues={{
+                    first: filteredVehicles.vehicleFull.length, 
+                    second: filteredVehicles.vehicleEmpty.length, 
+                    third: filteredVehicles.vehicleTeilbeladen.length
+                }}
                 animationTime={400}
             />
             
