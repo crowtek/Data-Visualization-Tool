a8055d877a9fb9b8c245eac428934bfea6552692
crowtek <prt3@hotmail.de>
2023-11-20 15:20:20 +0100

    added some best prectices to online User component

--- src/scenes/Dashboard/components/UserOnline.jsx
+++ src/scenes/Dashboard/components/UserOnline.jsx
@@ -10,14 +10,14 @@ const UserOnline = ({userData}) => {
     const colors = tokens(theme.palette.mode);
 
     const onlineUser = useMemo(() => {
-        return userData.filter((user)=> user.status === "online");
+        return userData?.filter((user)=> user.status === "online");
     },[userData])
 
     return (
         <Box gridColumn="span 3" gridRow="span 1" sx={boxStyle(colors)}>
             <StatBox
                 title="User Online"
-                subtitle= {onlineUser.length + " User"}
+                subtitle= {`${onlineUser.length} User`}
                 subtitleColor={colors.greenAccent[400]}
                 link = "calendar"
                 icon={<PersonOutlineOutlinedIcon sx={{ color: colors.greenAccent[400], fontSize: "30px" }} />}
