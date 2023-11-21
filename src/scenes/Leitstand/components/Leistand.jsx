8b2e2edebb733fdffc7abe006d4e3b11529dd159
crowtek <prt3@hotmail.de>
2023-11-21 19:27:15 +0100

    th space

--- src/scenes/Leitstand/components/Leistand.jsx
+++ src/scenes/Leitstand/components/Leistand.jsx
@@ -14,7 +14,7 @@ const LeitstandTable = ({ userData, screenSize }) => {
 
   const columns = useMemo(() => {
     return [
-      { field: "status", headerName: "Status", flex: 0.3, renderCell: ({ row: { status } }) => <VehicleStatus status={status} />, },
+      { field: "status", headerName: "Status", flex: 0.2, renderCell: ({ row: { status } }) => <VehicleStatus status={status} />, },
       { field: "vehicle", headerName: "Kennzeichen", flex: 0.5, id: "id" },
       { field: "parkPlace", headerName: "Standort", flex: 0.3, editable: true },
       { field: "cargo", headerName: "Ladeeinheit", flex: 0.5, editable: true },
