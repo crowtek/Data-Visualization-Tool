c676a424d5c9833ca45533af4f3a4d68e8e32fb9
crowtek <prt3@hotmail.de>
2024-02-12 18:08:51 +0100

    added the posibility to save hidden Column in the localstorage

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -8,21 +8,33 @@ import { TableStyle } from "../../styles/components/TableStyle";
 const Table = ({ tableData, tableColumns, tableName }) => {
   const [loading, setLoading] = useState(true);
   const [rows, setRows] = useState([]);
+  const [columnVisibilityModel, setColumnVisibilityModel] = useState({});   
   const [filterModel, setFilterModel] = useState({
     items: [],
   })
 
   // Save Filter
   const filterName = tableName + "Filter";
+  const filterColumnVis = tableName + "ColumnVis";
   const handleFilterModelChange = (newFilterModel) => {
     setFilterModel(newFilterModel);
     localStorage.setItem(filterName, JSON.stringify(newFilterModel));
+  };
+  
+  const handleColumnVisibilityModelChange = (newModel) => {
+    setColumnVisibilityModel(newModel);
+    localStorage.setItem(filterColumnVis, JSON.stringify(newModel));
   };
 
   useEffect(() => {
     const savedFilterModal = localStorage.getItem(filterName);
     if(savedFilterModal){
       setFilterModel(JSON.parse(savedFilterModal));
+    }
+  
+    const savedColumnVisibility = localStorage.getItem(filterColumnVis);
+    if (savedColumnVisibility) {
+      setColumnVisibilityModel(JSON.parse(savedColumnVisibility));
     }
   }, []);
 
@@ -45,8 +57,13 @@ const Table = ({ tableData, tableColumns, tableName }) => {
               autoPageSize
               disableRowSelectionOnClick
               loading={loading}
+
               filterModel={filterModel}
               onFilterModelChange={handleFilterModelChange}
+
+              columnVisibilityModel={columnVisibilityModel}
+              onColumnVisibilityModelChange={handleColumnVisibilityModelChange}
+
               slots={{
                 toolbar: GridToolbar,
               }}
