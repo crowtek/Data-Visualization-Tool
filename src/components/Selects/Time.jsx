04cc870882bc7375e2f4ed1656b2462d99e6e7d0
crowtek <prt3@hotmail.de>
2024-02-13 20:55:08 +0100

    created a Timepicker Component

---
+++ src/components/Selects/Time.jsx
@@ -0,0 +1,20 @@
+
+
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+
+const BasicTimePicker = () => {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <TimePicker
+        label="Basic time picker"
+        value={value}
+        onChange={handleChange}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
+  );
+}
+
+export default BasicTimePicker;
